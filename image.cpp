0a8014e4bbb76eb0dd345ada9317660f9a48141d
Zakharov Oleg <jsxft@hotmail.com>
2020-08-06 23:29:40 +0700

    fix SIMD

--- image.cpp
+++ image.cpp
@@ -72,13 +72,13 @@ ImageYUV ImageRGB::to_yuv_multithread() {
 ImageYUV ImageRGB::to_yuv_simd() {
     ImageYUV image(this->height, this->width);
 
-    for (int row = 0; row < this->height; ++row)
-        for (int col = 0; col < this->width; col += 16) {
+    for (int row = 0; row < this->height; ++row) {
+        for (int col = 0; col < this->width - 15; col += 16) {
             __m128i mask = _mm_setr_epi8(
-                    0x02,0x05,0x08,0x0b,
-                    0x01,0x04,0x07,0x0a,
-                    0x00,0x03,0x06,0x09,
-                    -1,-1,-1,-1);
+                    0x02, 0x05, 0x08, 0x0b,
+                    0x01, 0x04, 0x07, 0x0a,
+                    0x00, 0x03, 0x06, 0x09,
+                    -1, -1, -1, -1);
             __m128i clr[4], tmp[4];
 
             for (int i = 0; i < 4; ++i) {
@@ -95,7 +95,7 @@ ImageYUV ImageRGB::to_yuv_simd() {
             clr[2] = _mm_unpacklo_epi64(tmp[2], tmp[3]);
 
             __m128i v_lo[3], v_hi[3];
-            for(int i=0; i<3; ++i) {
+            for (int i = 0; i < 3; ++i) {
                 v_lo[i] = _mm_unpacklo_epi8(clr[i], _mm_setzero_si128());
                 v_hi[i] = _mm_unpackhi_epi8(clr[i], _mm_setzero_si128());
             }
@@ -106,7 +106,7 @@ ImageYUV ImageRGB::to_yuv_simd() {
                     112, -94, -18, -32768
             };
             __m128i yuv[3];
-            for(int i = 0; i < 3; ++i) {
+            for (int i = 0; i < 3; ++i) {
                 __m128i yuv_lo, yuv_hi;
                 yuv_lo = _mm_add_epi16(_mm_add_epi16(
                         _mm_mullo_epi16(v_lo[0], _mm_set1_epi16(m[i * 4])),
@@ -126,9 +126,9 @@ ImageYUV ImageRGB::to_yuv_simd() {
             }
 
             uint8_t y[16], u[16], v[16];
-            _mm_storeu_si128((__m128i*)y, yuv[0]);
-            _mm_storeu_si128((__m128i*)u, yuv[1]);
-            _mm_storeu_si128((__m128i*)v, yuv[2]);
+            _mm_storeu_si128((__m128i *) y, yuv[0]);
+            _mm_storeu_si128((__m128i *) u, yuv[1]);
+            _mm_storeu_si128((__m128i *) v, yuv[2]);
 
             for (int i = 0; i < 16; ++i) {
                 image.set_y(row, col + i, y[i]);
@@ -136,6 +136,13 @@ ImageYUV ImageRGB::to_yuv_simd() {
                 image.add_v(row / 2, (col + i) / 2, v[i] / 4);
             }
         }
+
+        for (int col = width & (-16); col < this->width; col++) {
+            image.set_y(row, col, (*this)[row][col].get_y());
+            image.add_u(row / 2, col / 2, (*this)[row][col].get_u() / 4);
+            image.add_v(row / 2, col / 2, (*this)[row][col].get_v() / 4);
+        }
+    }
 
     return image;
 }
